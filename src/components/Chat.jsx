80efbf585552fd7946e8d66ac8674e858dccef98
James Hawkins <jamesdonhawkins@gmail.com>
2024-09-25 11:41:23 -0700

    streaming works

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -45,12 +45,17 @@ function Chat({ token }) {
     setIsTyping(true);
 
     try {
+<<<<<<< HEAD
       const token = localStorage.getItem('token'); // Retrieve token from localStorage
       console.log('Sending message to server:', message);
       const response = await axios({
         method: 'post',
         url: `${process.env.REACT_APP_CHAT_API_URL}`,
         data: { content: message },
+=======
+      const res = await fetch(`${process.env.REACT_APP_CHAT_API_URL}`, {
+        method: 'POST',
+>>>>>>> fdbb303 (base azure deploy. not working.)
         headers: {
           'Authorization': `Bearer ${token}`,
           'Content-Type': 'application/json'
